12ae1b29c63c96d76dbc3d6360be1fda5071b749
Zhengde ZHOU <jack.zhou@richemont.com>
2018-03-14 18:54:15 +0800

    20130314

--- spring_jpa20180313/src/main/java/jack/springframework/orm/jpa/AppConfig.java
+++ spring_jpa20180313/src/main/java/jack/springframework/orm/jpa/AppConfig.java
@@ -1,15 +1,19 @@
 package jack.springframework.orm.jpa;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.orm.jpa.LocalEntityManagerFactoryBean;
 
 @Configuration
+@ComponentScan("jack.springframework.orm.jpa")
 public class AppConfig {
 
     @Bean
     public LocalEntityManagerFactoryBean myEmf() {
-        return new LocalEntityManagerFactoryBean();
+        LocalEntityManagerFactoryBean localEntityManagerFactoryBean = new LocalEntityManagerFactoryBean();
+        localEntityManagerFactoryBean.setPersistenceUnitName("");
+        return localEntityManagerFactoryBean;
     }
 
 }
