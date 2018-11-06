1bc4e94290af503b1f948890c88b549d7973c343
zzdzhou <zzd16000@163.com>
2018-11-06 08:31:46 +0800

    201811106

--- dive-in-spring-boot/spring-reactive/src/main/java/jack/springboot/reactive/ChainDataLoader.java
+++ dive-in-spring-boot/spring-reactive/src/main/java/jack/springboot/reactive/ChainDataLoader.java
@@ -1,7 +1,15 @@
 package jack.springboot.reactive;
 
+import java.util.concurrent.CompletableFuture;
+
 public class ChainDataLoader extends DataLoader {
 
-
-
+    @Override
+    protected void doLoad() {
+        CompletableFuture
+                .runAsync(this::loadConfigurations)
+                .thenRun(this::loadUsers)
+                .thenRun(this::loadOrders)
+                .whenComplete(() -> )
+    }
 }
